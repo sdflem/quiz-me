a30aebe79f25a12357f9f9a057cd8ee1d9488dce
Scott Fleming <sdflem@gmail.com>
2019-09-09 11:46:52 -0500

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
   get 'about', to: 'static_pages#about', as: 'about'
+  root to: redirect('/welcome', status: 302)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
