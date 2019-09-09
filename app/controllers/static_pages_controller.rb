dad2ba0f9f5c5c60ee85359e1170f6e76961bdc3
Scott Fleming <sdflem@gmail.com>
2019-09-09 11:35:40 -0500

    Added about page and added hyperlinks between the about and welcome pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,11 @@ class StaticPagesController < ApplicationController
       format.html { render :welcome }
     end
   end
+
+  def about
+    respond_to do |format|
+      format.html { render :about }
+    end
+  end
+
 end
